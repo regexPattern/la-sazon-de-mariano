45f290e25fe7defc424df98b819ff7e0496d233f
Carlos Castillo <47466248+regexPattern@users.noreply.github.com>
2023-11-16 20:41:15 -0300

    Solucionado el problema del main.py

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
 from flask import Flask
 
 from lib import router
-from lib.base_de_datos import BaseDeDatos
-
-BASE = {
-    "host": "localhost",
-    "user": "user",
-    "password": "password",
-    "database": "recetario",
-}
-
 
 def main():
     app = Flask(__name__, static_folder="static", template_folder="templates")
@@ -17,9 +8,7 @@ def main():
         "SECRET_KEY"
     ] = "SOsVMlbxQFy5LsbG0pUmXQ6PxxGHEcoO6BFoSyLXufyRL5TrQ9xfNRc27rjCIH3hAFu3yGaqXG1Kxba0EZfsGsJgUw0Jjbt2Agbc"
 
-    BaseDeDatos.configurar(**BASE)
     router.configurar(app)
-
     app.run("0.0.0.0", 5000, debug=True)
 
 
